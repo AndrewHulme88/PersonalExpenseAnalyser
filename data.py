95bdf3b72f30d151b4c8804e00d832265d2cfa8e
Andrew <andrew_hulme04@hotmail.com>
2026-02-16 20:33:32 +1100

    Added category breakdowns

--- data.py
+++ data.py
@@ -4,8 +4,10 @@ df = pd.read_csv("transactions.csv")
 df["Date"] = pd.to_datetime(df["Date"])
 df["Month"] = df["Date"].dt.to_period("M")
 
-monthly_expenses = df.groupby("Month")["Amount"].sum().reset_index()
+monthly_totals = df.groupby("Month")["Amount"].sum().reset_index()
+monthly_summary = df.groupby(["Month", "Category"])["Amount"].sum().unstack(fill_value=0)
 
-print(monthly_expenses)
+print(monthly_totals)
+print(monthly_summary)
 print(df.head())
 print(df.info())
