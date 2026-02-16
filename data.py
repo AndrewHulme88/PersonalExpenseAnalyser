6e1a49d855afbc9168f60ae47863846c1730fbd1
Andrew <andrew_hulme04@hotmail.com>
2026-02-16 20:40:45 +1100

    Calculated monthly expenses, and output simple bar chart

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("transactions.csv")
 df["Date"] = pd.to_datetime(df["Date"])
@@ -6,8 +7,14 @@ df["Month"] = df["Date"].dt.to_period("M")
 
 monthly_totals = df.groupby("Month")["Amount"].sum().reset_index()
 monthly_summary = df.groupby(["Month", "Category"])["Amount"].sum().unstack(fill_value=0)
+expenses = df[df["Amount"] < 0]
+monthly_expenses = expenses.groupby("Month")["Amount"].sum()
 
 print(monthly_totals)
 print(monthly_summary)
+print(monthly_expenses)
 print(df.head())
 print(df.info())
+
+monthly_expenses.plot(kind="bar", title="Monthly Expenses")
+plt.show()
