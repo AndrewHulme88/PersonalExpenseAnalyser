0e5d050a19b4a9e647af03cb2bf3e797f32f6b46
Andrew <andrew_hulme04@hotmail.com>
2026-02-16 20:29:00 +1100

    Added month column, and calculated monthly expenses

--- data.py
+++ data.py
@@ -2,5 +2,10 @@ import pandas as pd
 
 df = pd.read_csv("transactions.csv")
 df["Date"] = pd.to_datetime(df["Date"])
+df["Month"] = df["Date"].dt.to_period("M")
+
+monthly_expenses = df.groupby("Month")["Amount"].sum().reset_index()
+
+print(monthly_expenses)
 print(df.head())
 print(df.info())
