76d7dc27983fc718b1b15a8d36a683d3b5af9633
Andrew <andrew_hulme04@hotmail.com>
2026-02-17 15:47:36 +1100

    Added input to create new transactions

--- app.py
+++ app.py
@@ -5,11 +5,38 @@ st.title("Personal Expense Tracker")
 
 # Load Data
 df = pd.read_csv("transactions.csv")
-df["Date"] = pd.to_datetime(df["Date"])
+df["Date"] = pd.to_datetime(df["Date"], format="mixed")
 
 # Show Data
 st.header("Transactions")
-st.dataframe(df)
+display_df = df.copy()
+display_df["Date"] = display_df["Date"].dt.date
+st.dataframe(display_df)
+
+# Input
+st.header("Add New Transaction")
+
+with st.form("transaction_form"):
+    date = st.date_input("Date")
+    description = st.text_input("Description")
+    amount = st.number_input("Amount", step=0.01)
+    category = st.selectbox("Category", ["Food", "Transport", "Shopping", "Bills", "Entertainment", "Rent", "Other"])
+    
+    submitted = st.form_submit_button("Add Transaction")
+
+    if submitted:
+        new_row = pd.DataFrame([{
+            "Date": pd.to_datetime(date),
+            "Description": description,
+            "Amount": amount,
+            "Category": category
+        }])
+
+        df = pd.concat([df, new_row], ignore_index=True)
+        df.to_csv("transactions.csv", index=False)
+
+        st.success("Transaction added! Refreshing...")
+        st.rerun()
 
 # Monthly Spending
 expenses = df[df["Amount"] < 0]
